d9c07a8801213c14279d3f6d0f03119021941bc9
Ramon1710 <ramon.meyer@hotmail.de>
2026-05-11 12:21:14 +0000

    Initial Night-Whisper deploy

--- firebase.js
+++ firebase.js
@@ -6,12 +6,30 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
 
+const normalizeStorageBucket = (value, projectId) => {
+  if (!value) {
+    return `${projectId}.appspot.com`;
+  }
+
+  const normalizedValue = value
+    .replace(/^https?:\/\//i, '')
+    .replace(/\/.*$/, '');
+
+  if (/\.firebasestorage\.app$/i.test(normalizedValue)) {
+    return normalizedValue.replace(/\.firebasestorage\.app$/i, '.appspot.com');
+  }
+
+  return normalizedValue;
+};
+
+const projectId = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || 'affairgo-54ac0';
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || 'AIzaSyADdVt7T8OrJ3i3cYtazbbUaarBLcsq6xQ',
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || 'affairgo-54ac0.firebaseapp.com',
   databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL || 'https://affairgo-54ac0-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || 'affairgo-54ac0',
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || 'affairgo-54ac0.firebasestorage.app',
+  projectId,
+  storageBucket: normalizeStorageBucket(process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET, projectId),
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '75576714453',
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || '1:75576714453:web:56a0aae427fc301f3d2e5c',
   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || 'G-151QJPJZR9',
